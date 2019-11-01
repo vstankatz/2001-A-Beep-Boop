c043ef969dc57f236ad1bab282124e45eb259718
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-01 16:41:09 -0700

    fix back button, sets rule for negatives.

--- js/scripts.js
+++ js/scripts.js
@@ -4,38 +4,50 @@ var outputArray = [];
 function beepBoop(input) {
   console.log(input);
 
-  for (var i=0; i <= input; i++) {
-    var inputString = i.toString()
-    var inputArray = inputString.split("");
+  if (input >= 0) {
+    for (var i=0; i <= input; i++) {
+      var inputString = i.toString()
+      var inputArray = inputString.split("");
 
-    if (inputArray.includes("3")) {
-      outputArray.push("I'm Sorry Dave, I'm afraid I can't do that" + ", ")
-    } else if (inputArray.includes("2")) {
-      outputArray.push("Boop!" + ", ")
-    }else if (inputArray.includes("1")){
-      outputArray.push("Beep!" + ", ")
-    } else {
-      outputArray.push(inputString + ", ")
+      if (inputArray.includes("3")) {
+        outputArray.push("I'm Sorry Dave, I'm afraid I can't do that" + ", ")
+      } else if (inputArray.includes("2")) {
+        outputArray.push("Boop!" + ", ")
+      }else if (inputArray.includes("1")){
+        outputArray.push("Beep!" + ", ")
+      } else {
+        outputArray.push(inputString + ", ")
+      }
     }
+    $("#question").hide();
+    $("#answer").show();
+  } else {
+    alert("Please enter a positive number.");
   }
-  $("#question").hide();
-  $("#answer").show();
+
+
+
+
+
   console.log(outputArray);
 } //END OF FUNCTION!!!
 
+$(document).ready(function() {
+  $("#answer").hide();
+  var backBtn = document.getElementById("backBtn");
+  backBtn.onclick = function() {
+    $("#question").show();
+    $("#answer").hide();
+    document.getElementById("input").reset();
+    outputArray.length = 0;
+  }
 
 
 
 
   // User Logic
-  $(document).ready(function() {
-    $("#answer").hide();
-    var backBtn = document.getElementById("backBtn");
-    backBtn.onclick = function() {
-      $("#question").show();
-      $("#answer").hide();
-      document.getElementById("input").reset();
-    }
+
+
     $("form#input").submit(function(event) {
       event.preventDefault();
       var input = $("input#inputNumber").val();
