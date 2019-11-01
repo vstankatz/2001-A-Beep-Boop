bcfef2b3afa4e699d7b33e99c507ae11301c2fc7
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-01 09:45:14 -0700

    add function to return desired outputs for original input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,39 @@
 // Business Logic
+var input = 211;
+var outputArray = [];
 
+function beepBoop(input) {
+  inputString = input.toString()
+  var inputArray = inputString.split("");
+console.log(inputArray);
 
+    if (inputArray.includes("3") || ("2") || ("1")) {
+      if (inputArray.includes("3")) {
+        outputArray.push("I'm Sorry Dave, I'm afraid I can't do that.")
+      } else if (inputArray.includes("2")) {
+        outputArray.push("Boop!")
+        console.log(outputArray);
+      }else {
+        outputArray.push("Beep!")
+        console.log(outputArray);
+      }
 
+    }else if (input >= 0) {
+      outputArray.push(input)
+    } else {
+      console.log("error!!");
+    } console.log(outputArray);
 
+} //END OF FUNCTION!!!
 
+// for (var i in wordArray)
+// while (input >= 0) {
+//   input -1
+// }
+
+beepBoop(input)
 // User Logic
 $(document).ready(function() {
 
 })
+;
