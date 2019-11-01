34dbf62f6c73b7e3157563c1d3697360568ae2d1
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-01 11:03:56 -0700

    Create functioning loop with if/else to get desired count up to input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,38 @@
 // Business Logic
-var input = 211;
+var input = 12;
 var outputArray = [];
 
 function beepBoop(input) {
-  inputString = input.toString()
-  var inputArray = inputString.split("");
-console.log(inputArray);
 
-    if (inputArray.includes("3") || ("2") || ("1")) {
-      if (inputArray.includes("3")) {
-        outputArray.push("I'm Sorry Dave, I'm afraid I can't do that.")
-      } else if (inputArray.includes("2")) {
-        outputArray.push("Boop!")
-        console.log(outputArray);
-      }else {
-        outputArray.push("Beep!")
-        console.log(outputArray);
-      }
+  for (var i=0; i <= input; i++) {
+    var inputString = i.toString()
+    var inputArray = inputString.split("");
 
-    }else if (input >= 0) {
-      outputArray.push(input)
+    if (inputArray.includes("3")) {
+      outputArray.push("I'm Sorry Dave, I'm afraid I can't do that.")
+    } else if (inputArray.includes("2")) {
+      outputArray.push("Boop!")
+    }else if (inputArray.includes("1")){
+      outputArray.push("Beep!")
     } else {
-      console.log("error!!");
-    } console.log(outputArray);
+      outputArray.push(inputString)
+
+    }
+
+  }
+  console.log(outputArray);
+
 
 } //END OF FUNCTION!!!
 
 // for (var i in wordArray)
 // while (input >= 0) {
-//   input -1
-// }
+  //   input -1
+  // }
 
-beepBoop(input)
-// User Logic
-$(document).ready(function() {
+  beepBoop(input)
+  // User Logic
+  $(document).ready(function() {
 
-})
-;
+  })
+  ;
