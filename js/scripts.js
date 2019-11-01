ed08182986696850b856cd575bf07bd612b210ec
Veronica Stanley-Katz <vstankatz@gmail.com>
2019-11-01 16:22:29 -0700

    add back button, reset form, displays response over 2001 intro.

--- js/scripts.js
+++ js/scripts.js
@@ -1,81 +1,49 @@
 // Business Logic
-var input = 12;
 var outputArray = [];
 
 function beepBoop(input) {
+  console.log(input);
 
   for (var i=0; i <= input; i++) {
     var inputString = i.toString()
     var inputArray = inputString.split("");
 
     if (inputArray.includes("3")) {
-      outputArray.push("I'm Sorry Dave, I'm afraid I can't do that.")
+      outputArray.push("I'm Sorry Dave, I'm afraid I can't do that" + ", ")
     } else if (inputArray.includes("2")) {
-      outputArray.push("Boop!")
+      outputArray.push("Boop!" + ", ")
     }else if (inputArray.includes("1")){
-      outputArray.push("Beep!")
+      outputArray.push("Beep!" + ", ")
     } else {
-      outputArray.push(inputString)
+      outputArray.push(inputString + ", ")
     }
   }
+  $("#question").hide();
+  $("#answer").show();
   console.log(outputArray);
 } //END OF FUNCTION!!!
 
-function playVideo() {
-  var video = document.getElementByID("answerVideo")
-  var videoBtn = document.getElementByID("videoBtn")
-  video.muted(true);
-  videoBtn.addEventListener("click", function() {
-    video.muted(false);
-  })
 
 
-}
-// var video = getElementByID("answerVideo");
-// function video() {
-//
-// }
-// function onYouTubeIframeAPIReady() {
-//   var player;
-//   player = new YT.Player("YouTubeBackgroundVideoPlayer", {
-//     videoId: '{e-QFj59PON4}',
-//     width: auto,
-//     height: auto,
-//     playerVars: {
-//       playlist: '{e-QFj59PON4}',
-//         autoplay: 1,
-//         showinfo: 0,
-//         modestbranding: 1,
-//         loop: 1,
-//         rel:0,
-//         enablejsapi:1,
-//
-//     },
-//     events: {
-//       onReady: function(video) {
-//         video.target.mute();
-//         vido.target.setPlaybackQuality('hd180');
-//       },
-//       onStateChange: function(video) {
-//         if (video && video.data === 1) {
-//           var videoHolder =
-//           document.getElementByID("player")
-//           if (videoHolder && videoHolder.id) {
-//             videoHolder.classList.remove("LOADING PLACEHOLDER");
-//           }
-//         } else if (video && video.data === 0) {
-//           video.target.playVIdeo()
-//         }
-//       }
-//     }
-//   });
-// }
-//   onYouTubeIframeAPIReady()
-  beepBoop(input)
+
+
   // User Logic
   $(document).ready(function() {
+    $("#answer").hide();
+    var backBtn = document.getElementById("backBtn");
+    backBtn.onclick = function() {
+      $("#question").show();
+      $("#answer").hide();
+      document.getElementById("input").reset();
+    }
     $("form#input").submit(function(event) {
       event.preventDefault();
+      var input = $("input#inputNumber").val();
+      beepBoop(input)
+      $("#inputValue").text(input);
+      $("#output").text(outputArray.join(""));
+
+
     })
 
   });
